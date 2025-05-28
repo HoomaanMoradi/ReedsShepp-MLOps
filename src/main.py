569d79ec597a1587f9b7c2c1fdd5ae615cdc1e2f
hoomaan <hoomaanmoradi@outlook.com>
2025-05-28 15:56:15 +0200

    Initial commit including isort, black, and logging

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from logger import get_logger
+
+logger = get_logger(__name__)
+
+logger.info("Application started")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
